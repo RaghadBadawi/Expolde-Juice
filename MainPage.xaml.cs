5073b10cbc9e82eaae094ee9b0a4a8f64b53dc2b
rateeba12 <91503583+rateeba12@users.noreply.github.com>
2023-03-29 20:41:57 +0300

    Update MainPage.xaml.cs

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -19,8 +19,8 @@ namespace task1
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
             List<Item> itemList = new List<Item>
             {
-                      new Item { Name = "Item 1", Description = "Description for item 1", ImagePath = "Vector-Apple-Fruit-PNG-Photo.png" },
-                new Item { Name = "Item 2", Description = "Description for item 2", ImagePath = "item2.png" },
+                      new Item { Name = "Item 1", Description = "Description for item 1", ImagePath = "https://t4.ftcdn.net/jpg/04/44/69/07/360_F_444690746_s4c1CO3KL2mffD4ny8HHD4iVsCGiZ4dR.jpg" },
+                new Item { Name = "Item 2", Description = "Description for item 2", ImagePath = "https://t4.ftcdn.net/jpg/04/44/69/07/360_F_444690746_s4c1CO3KL2mffD4ny8HHD4iVsCGiZ4dR.jpg" },
                 new Item { Name = "Item 3", Description = "Description for item 3", ImagePath ="https://t4.ftcdn.net/jpg/04/44/69/07/360_F_444690746_s4c1CO3KL2mffD4ny8HHD4iVsCGiZ4dR.jpg" },
             };
 
