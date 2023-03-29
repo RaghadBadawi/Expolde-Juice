f8c1b9ac1e3228ed5a5d0747335a7669b27dfe4d
rateeba12 <91503583+rateeba12@users.noreply.github.com>
2023-03-29 18:45:11 +0300

    Add files via upload

---
+++ ingredientModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Firebase.Database;
+using Firebase.Database.Query;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace ExplodeJuice
+{
+	public class ingredientModel
+	{
+        public string image { get; set; }
+        public string IngredientName { get; set; }
+        public string ExpirationDate { get; set; }
+    }
+
+
+}
+
