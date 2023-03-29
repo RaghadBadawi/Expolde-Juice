e76a574b6852dcab207722dfeb846ef1a59ae2e7
RaghadBadawi <92644475+RaghadBadawi@users.noreply.github.com>
2023-03-29 22:17:57 +0300

    Add files via upload

---
+++ View Models/IngredientsViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+using System.Linq;
+using Newtonsoft.Json;
+using Firebase.Database;
+using System.Text;
+
+using add_ingredients.Models;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+namespace add_ingredients.View_models
+{
+    public class IngredientsViewModel : INotifyPropertyChanged
+    {
+        //private  FirebaseClient firebase;
+        public ObservableCollection<Ingredient> Ingredients { get; set; } = new ObservableCollection<Ingredient>();
+
+        public IngredientsViewModel()
+        {
+
+            // populate the fruit list
+            string appleImageSource = new FileImageSource { File = "Apple.png" };
+            Ingredients.Add(new Ingredient { IngredientName = "Apple", image = appleImageSource, IngredientType = "fruits" });
+            string bananaImageSource = new FileImageSource { File = "banana.png" };
+            Ingredients.Add(new Ingredient { IngredientName = "Banana", image = bananaImageSource, IngredientType = "fruits" });
+            // add more fruits as needed
+            //firebase = new FirebaseClient("https://explodejuice-6dbab-default-rtdb.firebaseio.com/");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        //public async Task<List<Ingredient>> GetIngredientsAsync()
+        //{
+        //    // Query Firebase database to retrieve ingredients data.
+        //    var ingredientsData = await firebase.Child("ingredients").OnceAsync<Ingredient>();
+
+        //    // Deserialize the retrieved data into a list of Ingredient objects.
+        //    var ingredients = ingredientsData.Select(d => JsonConvert.DeserializeObject<Ingredient>(d.Object.ToString())).ToList();
+
+        //    return ingredients;
+        //}
+    }
+}
---
+++ Views/add ingredients.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using add_ingredients.Models;
+using add_ingredients.View_models;
+using add_ingredients.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace add_ingredients.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class add_ingredients : ContentPage
+    {
+        public ObservableCollection<string> Items { get; set; }
+
+
+        public add_ingredients()
+        {
+            InitializeComponent();
+
+
+
+            Items = new ObservableCollection<string>
+            {
+                "Item 1",
+                "Item 2",
+                "Item 3",
+                "Item 4",
+                "Item 5"
+            };
+
+            //MyListView.ItemsSource = Items;
+        }
+
+        async void Handle_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            if (e.Item == null)
+                return;
+
+            await DisplayAlert("Item Tapped", "An item was tapped.", "OK");
+
+            //Deselect Item
+            ((ListView)sender).SelectedItem = null;
+        }
+
+        private void AddImageButtonClicked(object sender, EventArgs e)
+        {
+            addImageButton.Source = "check.png";
+        }
+
+        private void DoneButtonClicked(object sender, EventArgs e)
+        {
+            popupFrame.IsVisible = false;
+            // Do other things when the "Done" button is clicked
+        }
+
+        //private void Add_Clicked(object sender, EventArgs e)
+        //{
+
+        //}
+
+        // Create an instance of the IngredientsViewModel class
+
+
+        // Retrieve the list of ingredients from Firebase
+        //protected new async void OnAppearing()
+        //{
+        //    base.OnAppearing();   
+        //    IngredientsViewModel ingredientsViewModel = new IngredientsViewModel();
+
+        //    // Call the GetIngredientsAsync method and wait for the results.
+        //    List<Ingredient> ingredients = await ingredientsViewModel.GetIngredientsAsync();
+
+        //}
+
+
+
+    }
+}
