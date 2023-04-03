c07523b6f85a736b35576cd80d3d36a502441157
hanan zatar <90716074+hananzatar2001@users.noreply.github.com>
2023-04-03 22:18:35 +0300

    Add files via upload

---
+++ ViewModels/userViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Firebase.Database;
+using Firebase.Database.Query;
+using Xamarin.Forms;
+using Userprofile.Models;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Diagnostics;
+
+
+namespace Userprofile.ViewModels
+{
+    public class userViewModel 
+    {
+        FirebaseClient firebaseClient = new FirebaseClient("https://test-fe5e2-default-rtdb.firebaseio.com/");
+       
+        public userViewModel()
+        {
+
+            
+        }
+        public async Task<List<Users>> GetAll()
+        {
+            return (await firebaseClient.Child(nameof(Users)).OnceAsync<Users>()).Select(item => new Users
+            {
+                
+                FirstName = item.Object.FirstName,
+                LastName = item.Object.LastName,
+                Email = item.Object.Email,
+                Password = item.Object.Password,
+                Phone = item.Object.Phone,
+                ID=item.Key
+                
+               
+
+            }).ToList();
+        }
+        public async Task<Users> GetById(string id)
+        {
+            List<Users> users = await GetAll(); // Get all users from the database
+            Users user = users.FirstOrDefault(u => u.ID == id); // Find the user with the specified ID
+
+            return user;
+        }
+    }
+   
+    
+   
+}
+
+
+
---
+++ views/userprofile.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Userprofile.Models;
+using Userprofile.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Userprofile.views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class userprofile : ContentPage
+    {
+        userViewModel userviewmodel = new userViewModel();
+        public userprofile()
+        {
+            InitializeComponent();
+            
+        }
+         private void Returnar_row_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage());
+        }
+
+        private void Change_a_picture_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ImageButton_Clicked(object sender, EventArgs e)
+
+        {
+            //var button = (ImageButton)sender;
+            //var edit = (Users)button.BindingContext;
+            //edit.Password.InputTransparent = false;
+            //edit.FirstName.InputTransparent = false;
+            //edit.Email.InputTransparent = false;
+            //edit.Phone.InputTransparent = false;
+            //Save.IsVisible = true;
+        }
+        protected override async void OnAppearing()
+        {
+            string userId = "123"; // Replace with the ID of the user you want to retrieve
+            Users user = await userviewmodel.GetById(userId);
+
+            if (user != null)
+            {
+                // Display the user in your UI
+              
+                BindingContext = user;
+                //user.FirstName ="hanan";
+                //lastNameLabel.Text = user.LastName;
+                //emailLabel.Text = user.Email;
+                //phoneLabel.Text = user.Phone;
+            }
+            //    var user = await userviewmodel.GetAll();
+            //    userprofilelist.ItemsSource = null;
+            //    userprofilelist.ItemsSource = user;
+            //    userprofilelist.IsRefreshing = false;
+
+        }
+        
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+
+        }
+    }
+
+}
