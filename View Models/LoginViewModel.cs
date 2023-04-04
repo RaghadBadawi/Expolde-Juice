fec0f96084ba73ee1bab5addcda1f875deb4a409
RaghadBadawi <92644475+RaghadBadawi@users.noreply.github.com>
2023-04-04 03:36:03 +0300

    Add files via upload

---
+++ View Models/LoginViewModel.cs
@@ -0,0 +1,54 @@
+﻿using Firebase.Database;
+using Firebase.Database.Query;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using add_ingredients.Models;
+using System.Threading.Tasks;
+
+namespace add_ingredients.View_models
+{
+    public class LoginViewModel
+    {
+        public static FirebaseClient firebase = new FirebaseClient("https://test-fe5e2-default-rtdb.firebaseio.com/");
+
+        public static async Task<List<Users>> GetAllUser()
+        {
+            try
+            {
+                var userlist = (await firebase
+                .Child("Users")
+                .OnceAsync<Users>()).Select(item =>
+                new Users
+                {
+                    Email = item.Object.Email,
+                    Password = item.Object.Password
+                }).ToList();
+                return userlist;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine($"Error:{e}");
+                return null;
+            }
+        }
+        public static async Task<Users> GetUser(string email)
+        {
+            try
+            {
+                var allUsers = await GetAllUser();
+                await firebase
+                .Child("Users")
+                .OnceAsync<Users>();
+                return allUsers.Where(a => a.Email == email).FirstOrDefault();
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine($"Error:{e}");
+                return null;
+            }
+        }
+    }
+}
