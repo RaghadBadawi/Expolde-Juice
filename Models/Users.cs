0450de43cbc8427a8f7ff9e61b6ff910ab52d40b
Hala Assi <92643585+halaassi@users.noreply.github.com>
2023-04-06 20:56:34 +0300

    Add files via upload

---
+++ Models/Users.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace signup.Models
+{
+    public class Users
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+    }
+}
+
