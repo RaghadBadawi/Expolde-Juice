310f18b147c628d51c00bb6e7c9938d593814603
Hala Assi <92643585+halaassi@users.noreply.github.com>
2023-04-06 20:58:46 +0300

    Update SignUpVM.cs

--- ViewModel/SignUpVM.cs
+++ ViewModel/SignUpVM.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.ComponentModel;
 using Xamarin.Forms;
-using signup.Views;
+using ExplodeJuice.Views;
 
-namespace signup.ViewModel
+namespace ExplodeJuice.ViewModel
 {
     public class SignUpVM : INotifyPropertyChanged
     {
@@ -87,20 +87,16 @@ namespace signup.ViewModel
         }
         private async void SignUp()
         {
-            //null or empty field validation, check weather email and password is null or empty    
 
             if (string.IsNullOrEmpty(Email) || string.IsNullOrEmpty(Password))
                 await App.Current.MainPage.DisplayAlert("Empty Values", "Please enter Email and Password", "OK");
             else
             {
-                //call AddUser function which we define in Firebase helper class    
                 var user = await FirebaseHelper.AddUser(Email, FirstName, LastName, Password,Phone);
-                //AddUser return true if data insert successfuly     
                 if (user)
                 {
                     await App.Current.MainPage.DisplayAlert("SignUp Success", "", "Ok");
-                    //Navigate to Wellcom page after successfuly SignUp    
-                    //pass user email to welcom page    
+                 
                     await App.Current.MainPage.Navigation.PushAsync(new WellcomPage(Email));
                 }
                 else
