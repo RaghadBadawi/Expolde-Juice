80cfbcdc7bb8869a168e5436a46df114d4ee48c2
Hala Assi <92643585+halaassi@users.noreply.github.com>
2023-04-06 20:55:45 +0300

    Add files via upload

---
+++ Views/SignUpPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using signup.ViewModel;
+using Xamarin.Forms;
+
+namespace signup.Views
+{	
+	public partial class SignUpPage : ContentPage
+	{
+		SignUpVM signUpVM;
+
+		public SignUpPage ()
+		{
+
+            InitializeComponent();
+            signUpVM = new SignUpVM();
+            //set binding    
+            BindingContext = signUpVM;
+        }
+	}
+}
+
