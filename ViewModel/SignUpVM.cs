fd04e5104d34dea5d129e5ff93c6b3baf95d0b86
Hala Assi <92643585+halaassi@users.noreply.github.com>
2023-04-06 20:56:12 +0300

    Add files via upload

---
+++ ViewModel/SignUpVM.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using signup.Views;
+
+namespace signup.ViewModel
+{
+    public class SignUpVM : INotifyPropertyChanged
+    {
+        private string firstName;
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                firstName = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("FirstName"));
+            }
+        }
+        private string lastName;
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                lastName = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("LastName"));
+            }
+        }
+
+        private string email;
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                email = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Email"));
+            }
+        }
+        private string password;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                password = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Password"));
+            }
+        }
+
+        private string confirmpassword;
+        public string ConfirmPassword
+        {
+            get { return confirmpassword; }
+            set
+            {
+                confirmpassword = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("ConfirmPassword"));
+            }
+        }
+        private string phone;
+        public string Phone
+        {
+            get { return firstName; }
+            set
+            {
+                phone = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("phone"));
+            }
+        }
+        public Command SignUpCommand
+        {
+            get
+            {
+                return new Command(() =>
+                {
+                    if (Password == ConfirmPassword)
+                        SignUp();
+                    else
+                        App.Current.MainPage.DisplayAlert("", "Password must be same as above!", "OK");
+                });
+            }
+        }
+        private async void SignUp()
+        {
+            //null or empty field validation, check weather email and password is null or empty    
+
+            if (string.IsNullOrEmpty(Email) || string.IsNullOrEmpty(Password))
+                await App.Current.MainPage.DisplayAlert("Empty Values", "Please enter Email and Password", "OK");
+            else
+            {
+                //call AddUser function which we define in Firebase helper class    
+                var user = await FirebaseHelper.AddUser(Email, FirstName, LastName, Password,Phone);
+                //AddUser return true if data insert successfuly     
+                if (user)
+                {
+                    await App.Current.MainPage.DisplayAlert("SignUp Success", "", "Ok");
+                    //Navigate to Wellcom page after successfuly SignUp    
+                    //pass user email to welcom page    
+                    await App.Current.MainPage.Navigation.PushAsync(new WellcomPage(Email));
+                }
+                else
+                    await App.Current.MainPage.DisplayAlert("Error", "SignUp Fail", "OK");
+
+            }
+        }
+    }
+}
+
