9411b69ccf097be31efe5bad9be6f33092ada0de
Hala Assi <92643585+halaassi@users.noreply.github.com>
2023-04-06 20:57:25 +0300

    Add files via upload

---
+++ Models/recipes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ExplodeJuice
+{
+	public class recipes
+	{
+        public int RecipeID { get; set; }
+        public string RecipeName { get; set; }
+        public string Ingredients { get; set; }
+      
+    }
+}
+
