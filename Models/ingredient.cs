335611d15e398e2012dc005368c0f5747f0291fe
Hala Assi <92643585+halaassi@users.noreply.github.com>
2023-04-06 20:57:07 +0300

    Add files via upload

---
+++ Models/ingredient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ExplodeJuice
+{
+	public class ingredient
+	{
+		public int IngredientID { get; set; }
+        public string IngredientName { get; set; }
+        public string IngredientType { get; set; }
+        public string QuantityInStock { get; set; }
+        public string ExpirationDate { get; set; }
+        public string image { get; set; }
+    }
+
+
+}
+
