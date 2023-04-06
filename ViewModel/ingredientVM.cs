955f1df69bb4cce8b53521d24bb0d1a5f317c136
Hala Assi <92643585+halaassi@users.noreply.github.com>
2023-04-06 20:57:57 +0300

    Add files via upload

---
+++ ViewModel/ingredientVM.cs
@@ -0,0 +1,17 @@
+﻿using Firebase.Database;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExplodeJuice
+{
+	public class ingredientVM
+	{
+        FirebaseClient firebaseClient = new FirebaseClient("https://explodejuice-6dbab-default-rtdb.firebaseio.com/");
+
+    }
+}
+
