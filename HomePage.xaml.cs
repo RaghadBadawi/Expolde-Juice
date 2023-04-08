cc5ce40eaba91d2ff526177500bc0165d2be943e
RaghadBadawi <92644475+RaghadBadawi@users.noreply.github.com>
2023-04-08 22:50:40 +0300

    EJ-49 HomePage&Logo

---
+++ HomePage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using add_ingredients.Models;
+namespace add_ingredients.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : ContentPage
+    {
+        Users me;
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+        public HomePage(Users user)
+        {
+            InitializeComponent();
+            me = new Users();
+            me = user;
+        }
+
+        private void startCreateButtonClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new selectIngredients());
+        }
+
+        private void profileButtonClicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new userprofile(me));
+        }
+    }
+}
