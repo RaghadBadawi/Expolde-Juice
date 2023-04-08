4250408b4f29ca505c897cce474863536bd08c26
Hala Assi <92643585+halaassi@users.noreply.github.com>
2023-04-08 22:38:41 +0300

    Add files via upload

---
+++ AdminModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace add_ingredients.Models
+{
+	public class AdminModels
+	{
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+    }
+}
+
+
