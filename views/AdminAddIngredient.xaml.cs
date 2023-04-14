fe9b3186ffc48c3e531389856a73f68d8f8b93f5
rateeba12 <91503583+rateeba12@users.noreply.github.com>
2023-04-15 00:23:59 +0300

    Update AdminAddIngredient.xaml.cs

--- views/AdminAddIngredient.xaml.cs
+++ views/AdminAddIngredient.xaml.cs
@@ -1,20 +1,46 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
-
-namespace Userprofile.views
+namespace InsertIngredients
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class AdminAddIngredient : ContentPage
+    public partial class InsertIngredient : ContentPage
     {
-        public AdminAddIngredient()
+        public Entry EntryName => Name;
+        public Editor Descreption => description;
+
+        public InsertIngredient(ingredientModel ingredient)
+        {
+            InitializeComponent();
+            if (ingredient != null)
+            {
+                Name.Text = ingredient.Name;
+                description.Text = ingredient.Description;
+            }
+        }
+
+        public InsertIngredient()
         {
             InitializeComponent();
         }
+
+   
+
+
+
+
+        private void save_Clicked(object sender, EventArgs e)
+        {
+            ingredientModel item = new ingredientModel();
+            ingredientRepository ingredientRepository = new ingredientRepository();
+            _ = ingredientRepository.AddTheIngredient(Name.Text, description.Text);
+            Name.Text = string.Empty;
+            description.Text = string.Empty;
+            _ = DisplayAlert("Success", "Ingredient Added Successfully", "OK");
+
+        }
+
     }
 }
