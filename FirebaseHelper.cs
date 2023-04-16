e202d2ddf7cacbea106f2d1f516ba62d56387570
hanan zatar <90716074+hananzatar2001@users.noreply.github.com>
2023-04-16 17:49:51 +0300

    Add files via upload

---
+++ FirebaseHelper.cs
@@ -0,0 +1,54 @@
+﻿using Firebase.Database;
+using Firebase.Database.Query;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Userprofile.Models;
+
+namespace Userprofile.ViewModel
+{
+    public class FirebaseHelper
+    {
+        public static FirebaseClient firebase = new FirebaseClient("https://explodejuice-6dbab-default-rtdb.firebaseio.com/");
+
+        public FirebaseHelper()
+        {
+        }
+        public static async Task<bool> AddUser(string email, string firstName, string lastName, string password, string phone)
+        {
+            try
+            {
+
+
+                await firebase
+                .Child("Users")
+                .PostAsync(new Users() { Email = email, FirstName = firstName, LastName = lastName, Password = password, Phone = phone });
+                return true;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine($"Error:{e}");
+                return false;
+            }
+        }
+
+
+        public async Task Updateuser(string email, string firstName, string lastName, string phone, string password)
+{
+    var firebase = new FirebaseClient("your firebase database url");
+    var admin = (await firebase.Child("Admins").OnceAsync<Users>()).Where(a => a.Object.Email == email).FirstOrDefault();
+    if (admin != null)
+    {
+        admin.Object.FirstName = firstName;
+        admin.Object.LastName = lastName;
+        admin.Object.Phone = phone;
+        admin.Object.Password = password;
+        await firebase.Child("Admins").Child(admin.Key).PutAsync(admin.Object);
+    }
+}
+
+    }
+}
---
+++ userprofile.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Userprofile.Models;
+using Userprofile.ViewModels;
+using Userprofile.ViewModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Firebase.Database;
+using Firebase.Database.Query;
+
+
+namespace Userprofile.views
+{
+    public partial class userprofile : ContentPage
+    {
+        public Entry FirstNameEntry => FirstName;
+        public Entry EmailEntry => Email;
+        public Entry PhoneEntry => Phone;
+        public Entry PasswordEntry => Password;
+        public Entry LastNameEntry => LastName;
+
+
+
+        UserViewModel userviewmodel = new UserViewModel();
+        public userprofile(Users user)
+        {
+            InitializeComponent();
+            if (user != null) { 
+            FirstName.Text = user.FirstName;
+            LastName.Text = user.LastName;
+            Email.Text = user.Email;
+            Phone.Text = user.Phone;
+            Password.Text = user.Password;
+
+            }
+        }
+        public userprofile()
+        {
+            InitializeComponent();
+        }
+        //protected override async void OnAppearing()
+        //{
+        // var user= await userviewmodel.GetAll();
+          
+        //}
+         private void Returnar_row_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage());
+        }
+
+        private void Change_a_picture_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ImageButton_Clicked(object sender, EventArgs e)
+
+        {
+            
+            Password.InputTransparent = false;
+            FirstName.InputTransparent = false;
+            LastName.InputTransparent = false;
+            Email.InputTransparent = false;
+            Phone.InputTransparent = false;
+            Save.IsVisible = true;
+        }
+
+        private async void Save_Clicked(object sender, EventArgs e)
+        {
+
+            FirebaseHelper firebaseHelper = new FirebaseHelper();
+            await firebaseHelper.Updateuser(EmailEntry.Text, FirstNameEntry.Text, LastNameEntry.Text, PhoneEntry.Text, PasswordEntry.Text);
+            Save.IsVisible = false;
+            await DisplayAlert("Success", "Information Updated Successfully", "OK");
+            Password.InputTransparent = true;
+            FirstNameEntry.InputTransparent = true;
+
+            LastNameEntry.InputTransparent = true;
+            Email.InputTransparent = true;
+            Phone.InputTransparent = true;
+        }
+
+    }
+
+}
